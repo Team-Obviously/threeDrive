801dc0e1ff1a1687c98160f6007af500f552fd2d
Raghav Jindal <raghavjindal0212@gmail.com>
2024-12-06 23:48:49 +0530

    init okto

--- client/packages/nextjs/app/layout.tsx
+++ client/packages/nextjs/app/layout.tsx
@@ -1,17 +1,21 @@
 import "@rainbow-me/rainbowkit/styles.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({ title: "Scaffold-ETH 2 App", description: "Built with 🏗 Scaffold-ETH 2" });
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
       <body>
         <ThemeProvider enableSystem>
-          <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID!}>
+            <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+          </GoogleOAuthProvider>
         </ThemeProvider>
       </body>
     </html>
