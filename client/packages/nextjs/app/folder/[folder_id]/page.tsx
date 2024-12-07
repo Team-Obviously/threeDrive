187289b2298b9244e58b917e8534d95a133d3e83
arya <aryaajitnair@gmail.com>
2024-12-08 03:18:05 +0530

    remove extra search fields

--- client/packages/nextjs/app/folder/[folder_id]/page.tsx
+++ client/packages/nextjs/app/folder/[folder_id]/page.tsx
@@ -402,31 +402,6 @@ export default function FolderPage() {
           <h1 className="text-xl font-semibold">{folder?.path}</h1>
         </div>
 
-        <div className="relative w-96">
-          <Input
-            type="text"
-            placeholder="Search files..."
-            className="pl-3 pr-10"
-            onChange={e => {
-              console.log(e.target.value);
-            }}
-          />
-          <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-            <svg
-              className="h-5 w-5 text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        </div>
-
         <div className="relative flex flex-row gap-4">
           <Button className="" variant="default" size="icon" onClick={fetchFolder}>
             <RefreshCw className="h-6 w-6" />
